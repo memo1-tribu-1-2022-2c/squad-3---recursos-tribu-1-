26011bc0258f8f1afbcdb5d61e82beaaf1669a6e
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2022-12-03 13:19:17 -0300

    Refactor ResourceService

--- src/main/java/ar/uba/fi/recursos/service/ResourceService.java
+++ src/main/java/ar/uba/fi/recursos/service/ResourceService.java
@@ -1,9 +1,6 @@
 package ar.uba.fi.recursos.service;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +9,9 @@ import ar.uba.fi.recursos.model.Resource;
 @Service
 public class ResourceService {
 
+    static final String url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos";
+
     public List<Resource> getAllResources() {
-        String url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos";
-        
         RestTemplate restTemplate = new RestTemplate();
         Resource[] resourcesRaw = restTemplate.getForObject(url, Resource[].class);
         
@@ -22,12 +19,11 @@ public class ResourceService {
             return Collections.emptyList();
         }
 
-        List<Resource> resources = Arrays.asList(resourcesRaw);
-        return resources;
+        return Arrays.asList(resourcesRaw);
     }
 
     public Optional<Resource> findById(Long resourceId) {
         List<Resource> resources = this.getAllResources();
-        return resources.stream().filter(r -> r.getId() == resourceId).findAny();
+        return resources.stream().filter(r -> r.getId().equals(resourceId)).findAny();
     }
 }
