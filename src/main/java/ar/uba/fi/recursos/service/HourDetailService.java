6c20d9999126810d61d59b8fb7dc88937b9f843a
SantiRuizMazzo <santimazzo98@gmail.com>
2022-12-03 10:23:10 -0300

    refactor: created findXXX and deleteById methods for HourDetailService

--- src/main/java/ar/uba/fi/recursos/service/HourDetailService.java
+++ src/main/java/ar/uba/fi/recursos/service/HourDetailService.java
@@ -1,5 +1,17 @@
 package ar.uba.fi.recursos.service;
 
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import ar.uba.fi.recursos.dtos.TaskData;
 import ar.uba.fi.recursos.exceptions.InvalidDatesException;
 import ar.uba.fi.recursos.exceptions.InvalidTypeException;
@@ -8,29 +20,20 @@ import ar.uba.fi.recursos.model.HourDetailStatus;
 import ar.uba.fi.recursos.model.TimeRegister;
 import ar.uba.fi.recursos.model.TimeRegisterTypeOfActivity;
 import ar.uba.fi.recursos.repository.HourDetailRepository;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class HourDetailService {
 
     @Autowired
     private HourDetailRepository hourDetailRepository;
-
     @Autowired
     private ResourceService resourceService;
 
     public HourDetail createHourDetail(HourDetail hourDetail) throws Throwable {
 
-        if (!verifyDates(hourDetail)) {
+        // fix: manejar errores sin tirar excepciones
+
+        if (!hasValidPeriod(hourDetail)) {
             throw new InvalidDatesException("Las fechas del parte no son validas o se superponen con unas existentes");
         }
 
@@ -38,53 +41,54 @@ public class HourDetailService {
             throw new InvalidTypeException("El recurso no existe");
         }
 
-        hourDetail.setStatus(HourDetailStatus.BORRADOR);
+        hourDetail.setStatus(HourDetailStatus.DRAFT);
         hourDetail.setTimeRegisters(new ArrayList<TimeRegister>());
         return hourDetailRepository.save(hourDetail);
     }
 
-    public boolean verifyDates(HourDetail hourDetail) {
+    public boolean hasValidPeriod(HourDetail hourDetail) {
         Date startDate = Date.from(hourDetail.getStartTime().atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(startDate);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+
         switch (hourDetail.getType()) {
             case SEMANAL: {
-                if (!((cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY))) {
+                if (!((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY))) {
                     return false;
                     // throw new InvalidDatesException("La fecha especificada no es un lunes: " +
                     // startDate);
                 }
-                cal.add(Calendar.DAY_OF_MONTH, 6);
-                hourDetail.setEndTime(cal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                calendar.add(Calendar.DAY_OF_MONTH, 6);
+                hourDetail.setEndTime(calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
                 break;
             }
             case QUINCENAL: {
-                if (!((cal.get(Calendar.DAY_OF_MONTH) == 1)) && !((cal.get(Calendar.DAY_OF_MONTH) == 16))) {
+                if (!((calendar.get(Calendar.DAY_OF_MONTH) == 1)) && !((calendar.get(Calendar.DAY_OF_MONTH) == 16))) {
                     return false;
                     // throw new InvalidDatesException("La fecha especificada no es un 1 o 16 del
                     // mes: " + startDate);
                 }
 
-                if ((cal.get(Calendar.DAY_OF_MONTH) == 1)) {
-                    cal.add(Calendar.DAY_OF_MONTH, 14);
-                    hourDetail.setEndTime(cal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                if ((calendar.get(Calendar.DAY_OF_MONTH) == 1)) {
+                    calendar.add(Calendar.DAY_OF_MONTH, 14);
+                    hourDetail.setEndTime(calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
                     break;
                 }
-                int max_day = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-                cal.set(Calendar.DAY_OF_MONTH, max_day);
-                hourDetail.setEndTime(cal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                int max_day = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                calendar.set(Calendar.DAY_OF_MONTH, max_day);
+                hourDetail.setEndTime(calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
                 break;
             }
             case MENSUAL: {
 
-                if (!((cal.get(Calendar.DAY_OF_MONTH) == 1))) {
+                if (!((calendar.get(Calendar.DAY_OF_MONTH) == 1))) {
                     return false;
                     // throw new InvalidDatesException("La fecha especificada no es el primer día
                     // del mes: " + startDate);
                 }
-                int max_day = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-                cal.set(Calendar.DAY_OF_MONTH, max_day);
-                hourDetail.setEndTime(cal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                int max_day = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                calendar.set(Calendar.DAY_OF_MONTH, max_day);
+                hourDetail.setEndTime(calendar.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
                 break;
             }
             default: {
@@ -134,7 +138,19 @@ public class HourDetailService {
         return totalHours;
     }
 
-    public Optional<HourDetail> findById(Long hourDetailId) {
-        return this.hourDetailRepository.findById(hourDetailId);
+    public Optional<HourDetail> findById(Long id) {
+        return hourDetailRepository.findById(id);
+    }
+
+    public List<HourDetail> findByWorkerId(Long workerId) {
+        return hourDetailRepository.findByWorkerId(workerId);
+    }
+
+    public List<HourDetail> findAll() {
+        return hourDetailRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        hourDetailRepository.deleteById(id);
     }
 }
